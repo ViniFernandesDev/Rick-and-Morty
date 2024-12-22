8c461445b0cec88ee5c913fac26b23d4a2d1d2a1
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-12-22 12:55:46 -0300

    config eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,35 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      'airbnb-base',
+      ...tseslint.configs.recommended,
+      'plugin:react-hooks/recommended',
+      'prettier'
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'prettier/prettier': 'error'
+    }
+  }
 )
