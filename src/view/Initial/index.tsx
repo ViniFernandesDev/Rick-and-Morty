b53ffbb891f3de65546fa8ab93b0f9d968056c30
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-12-26 12:08:03 -0300

    style: mobile initial

--- src/view/Initial/index.tsx
+++ src/view/Initial/index.tsx
@@ -9,16 +9,16 @@ export function Initial() {
   return (
     <div className="flex items-center justify-center h-full">
       <div className="text-mortyYellow flex items-center justify-center flex-col  animate-logo">
-        <div className="flex flex-col items-center text-center gap-6">
+        <div className="flex flex-col items-center text-center gap-2 sm:gap-6">
           <Logo />
 
-          <p className="font-getSchwifty max-w-[630px] text-white text-3xl leading-[60px] tracking-[3px]">
+          <p className="font-getSchwifty px-2 max-w-[630px] text-white text-1xl sm:text-3xl leading-9 sm:leading-[60px] tracking-[3px]">
             Explore os heróis, vilões e criaturas do multiverso de Rick and
             Morty.
           </p>
         </div>
 
-        <Button className="mt-16" onClick={() => navigate('/home')}>
+        <Button className="mt-8 sm:mt-16" onClick={() => navigate('/home')}>
           QUERO COMEÇAR
         </Button>
       </div>
